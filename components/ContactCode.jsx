16a74c29b984e6451703150e72be561bcdcb73ab
Alejandro Lopez Monzon <alexlm78@gmail.com>
2024-04-25 08:42:13 -0600

    fix(contact): Fix the contact email to kreaker.me domain

--- components/ContactCode.jsx
+++ components/ContactCode.jsx
@@ -1,73 +1,73 @@
-import styles from '../styles/ContactCode.module.css';
+import styles from "../styles/ContactCode.module.css";
 
 const contactItems = [
-  {
-    social: 'Email',
-    link: 'alexlm78@gmail.com',
-    href: 'mailto:alexlm78@gmail.com',
-  },
-  {
-    social: 'LinkedIn',
-    link: 'linkedin.com/in/alexlm78',
-    href: 'https://www.linkedin.com/in/alexlm78/',
-  },
-  {
-    social: 'GitHub',
-    link: 'github.com/alexlm78',
-    href: 'https://github.com/alexlm78',
-  },
-  {
-    social: 'Twitter',
-    link: 'twitter.com/alexlm78',
-    href: 'https://twitter.com/alexlm78',
-  },
-  {
-    social: 'Instagram',
-    link: 'instagram.com/alexlm78',
-    href: 'https://www.instagram.com/alexlm78/',
-  },
-  {
-    social: 'StackOverflow',
-    link: 'stackoverflow.com/users/9525502/',
-    href: 'https://stackoverflow.com/users/9525502/alejandro-lopez-monzon',
-  },
-  {
-    social: 'Website',
-    link: 'kreaker.dev',
-    href: 'https://kreaker.dev',
-  },
+	{
+		social: "Email",
+		link: "alejandro@kreaker.me",
+		href: "mailto:alejandro@kreaker.me",
+	},
+	{
+		social: "LinkedIn",
+		link: "linkedin.com/in/alexlm78",
+		href: "https://www.linkedin.com/in/alexlm78/",
+	},
+	{
+		social: "GitHub",
+		link: "github.com/alexlm78",
+		href: "https://github.com/alexlm78",
+	},
+	{
+		social: "Twitter",
+		link: "twitter.com/alexlm78",
+		href: "https://twitter.com/alexlm78",
+	},
+	{
+		social: "Instagram",
+		link: "instagram.com/alexlm78",
+		href: "https://www.instagram.com/alexlm78/",
+	},
+	{
+		social: "StackOverflow",
+		link: "stackoverflow.com/users/9525502/",
+		href: "https://stackoverflow.com/users/9525502/alejandro-lopez-monzon",
+	},
+	{
+		social: "Website",
+		link: "kreaker.dev",
+		href: "https://kreaker.dev",
+	},
 ];
 
 const ContactCode = () => {
-  return (
-    <div className={styles.code}>
-       <p className={styles.line}>
-       tag: <a>production</a>
-        </p>
-      <p className={styles.line}>
-        <span>Alejandro</span>&#58;
-      </p>
-      <p className={styles.line}>
-        &nbsp;&nbsp;&nbsp;&#8212; <span>socials</span>&#58;
-      </p>
-      {contactItems.slice(0, 8).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{' '}
-          <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
-          </a>
-        </p>
-      ))}
-      {contactItems.slice(8, contactItems.length).map((item, index) => (
-        <p className={styles.line} key={index}>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{' '}
-          <a href={item.href} target="_blank" rel="noopener">
-            {item.link}
-          </a>
-        </p>
-      ))}
-    </div>
-  );
+	return (
+		<div className={styles.code}>
+			<p className={styles.line}>
+				tag: <a>production</a>
+			</p>
+			<p className={styles.line}>
+				<span>Alejandro</span>&#58;
+			</p>
+			<p className={styles.line}>
+				&nbsp;&nbsp;&nbsp;&#8212; <span>socials</span>&#58;
+			</p>
+			{contactItems.slice(0, 8).map((item, index) => (
+				<p className={styles.line} key={index}>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{" "}
+					<a href={item.href} target="_blank" rel="noopener">
+						{item.link}
+					</a>
+				</p>
+			))}
+			{contactItems.slice(8, contactItems.length).map((item, index) => (
+				<p className={styles.line} key={index}>
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{item.social}:{" "}
+					<a href={item.href} target="_blank" rel="noopener">
+						{item.link}
+					</a>
+				</p>
+			))}
+		</div>
+	);
 };
 
 export default ContactCode;
