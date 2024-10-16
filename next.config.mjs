b55ad876bcd0caaf2c9a265f9d11b904c715e7bd
Alejandro Lopez Monzon <alexlm78@gmail.com>
2024-10-15 19:08:38 -0600

    cleaning

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['aceternity.com', 'images.unsplash.com', 'assets.aceternity.com'],
-  },};
-
-export default nextConfig;
